cde0241eefbd1de75eb5b8c2fe644be19af27aaa
sanguineimpasse <10geneius@gmail.com>
2024-10-11 17:05:05 +0800

    modified default template

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <div class="container justify-content-center align-items-center" style="width: 50vw;">
+        <h1 class="text-center">Hello world!</h1>
+        <button class="btn btn-sm btn-success">This is a button</button>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/hello');
+});
+
+Route::get('/hello', function () {
+    return view('hello');
 });
